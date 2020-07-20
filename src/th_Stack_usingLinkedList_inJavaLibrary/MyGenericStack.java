3eb367e80ba7a0c47efbccc9a8791e93df0c7fc2
tungchihuy95 <tungjpvn95@gmail.com>
2020-07-20 13:43:42 +0700

    GenericStack

---
+++ src/th_Stack_usingLinkedList_inJavaLibrary/MyGenericStack.java
@@ -0,0 +1,36 @@
+package th_Stack_usingLinkedList_inJavaLibrary;
+
+import java.util.EmptyStackException;
+import java.util.LinkedList;
+
+public class MyGenericStack<T> {
+    private LinkedList<T> stack;
+
+    public MyGenericStack() {
+        stack = new LinkedList<>();
+    }
+
+    public void push(T element) {
+        stack.addFirst(element);
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return stack.removeFirst();
+    }
+
+    public int size() {
+        return stack.size();
+    }
+
+    public boolean isEmpty() {
+        if (stack.size() == 0) {
+            return true;
+        }
+        return false;
+    }
+
+
+}
