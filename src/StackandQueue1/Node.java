b140fadfd75b00bbd3bb25bebab64a565b491127
tungchihuy95 <tungjpvn95@gmail.com>
2020-07-20 11:34:09 +0700

    StackandQueue

---
+++ src/StackandQueue1/Node.java
@@ -0,0 +1,13 @@
+package StackandQueue1;
+
+public class Node {
+    public int key;
+    public Node next;
+
+    public Node(int key) {
+        this.key = key;
+        this.next = next;
+    }
+
+
+}
